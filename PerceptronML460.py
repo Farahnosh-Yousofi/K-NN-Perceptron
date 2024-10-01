8938895fd63e897004804e67ec9bf6a221e07592
Farahnosh-Yousofi <80102585+Farahnosh-Yousofi@users.noreply.github.com>
2024-09-30 20:09:32 -0400

    Update_weight function and train weight function added and tested
    Update weight function updates the weight if the prediction of the label is wrong with any of the rows of data in the dataset. Train weight will train the code for different attempts till the desired weight is achieved that could classify the feature vectors correctly or predicts the label of the features correctly

--- PerceptronML460.py
+++ PerceptronML460.py
@@ -2,6 +2,7 @@
 # Defining the function predict
 def predict(rows, weights):
     '''Returns the list of dot products between elements of rows and weights'''
+    
     return (rows[i]*weights[i] for i in range(len(weights)))
 
 
@@ -21,3 +22,34 @@ for row in dataset:
                                           "Correct" if prediction >= 0
                                           else "Wrong"))
 
+
+
+# Weights update function
+# w <- w + yx
+def weight_update(row, weights, labels):
+    '''Updates the weights based on the given row, labels and weights'''
+    return  [(weights[i]+ labels*row[i]) for i in range(len(weights))]
+
+
+# Estimate the perceptron weights using peceptron
+def train_weights(train, n_epoch):
+    """Train weights for the given number of epochs"""
+    weights = [0.0 for i in range(len(train[0][0]))]
+    print(weights)
+    for epoch in range(n_epoch):
+        m = 0
+        for row in train:
+            prediction = sum(predict(row[0], weights))*row[1]
+            if prediction <= 0:
+                weights = weight_update(row[0], weights, row[1])
+                print(weights)
+                m += 1
+        print("Epoch %d, Misclassified samples=%d" % (epoch, m))
+        if m == 0:
+            break
+    print(weights)
+    return weights
+
+n_epoch = 5
+weights = train_weights(dataset, n_epoch)
+print(weights)
